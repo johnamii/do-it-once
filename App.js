fef560b3827961634b6e3097fa611c1629bdffe7
John Flanagan <81453889+johnamii@users.noreply.github.com>
2024-03-03 13:14:38 -0800

    write root navigation

--- App.js
+++ App.js
@@ -2,13 +2,23 @@ import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Nav
+import BottomNavBar from './navigation/BottomNavbar';
+// Components
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import SignOut from './components/SignOut';
 
+const RootStack = createNativeStackNavigator();
+
 export default function App() {
 
   const [currentUser, setCurrentUser] = useState(null);
+  let tempLoggedIn = true; // replace with user authentication logic when ready
 
   console.log('currentUser', currentUser?.email)
 
@@ -25,11 +35,25 @@ export default function App() {
   }, [currentUser])
 
   return (
-    <View style={styles.container}>
-      <SignUp user={currentUser} setUser={setCurrentUser} />
-      {/* <SignIn user={currentUser} setUser={setCurrentUser} /> */}
-      {/* <SignOut user={currentUser} /> */}
-    </View>
+    <NavigationContainer>
+        <RootStack.Navigator>
+          {
+            !tempLoggedIn ? (
+              <>
+                {/* IF NOT LOGGED IN AUTOMATICALLY, SHOW STACK */}
+                <RootStack.Screen name="Sign Up" component={SignUp}/>
+                <RootStack.Screen name="Sign In" component={SignIn}/>
+                <RootStack.Screen name="Sign Out" component={SignOut}/>
+              </>
+            ) : (
+              <>
+                {/* OTHERWISE, SHOW BOTTOMTABNAVIGATOR AS ROOT COMPONENT FOR MAIN APP */}
+                <RootStack.Screen name="Main App" component={BottomNavBar}/>
+              </>
+            )
+          }
+        </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
 
