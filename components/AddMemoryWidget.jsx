fb8dc33f0b008ebe82c9ce457e87488ecc130a41
Dwight Bumgarner <dwightbumgarner@gmail.com>
2024-03-03 18:51:53 -0800

    Added UI to memory widget

--- components/AddMemoryWidget.jsx
+++ components/AddMemoryWidget.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput, Button, Alert } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Modal, TextInput, Alert, Image, Dimensions, ActionSheetIOS } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { getAuth } from 'firebase/auth';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { db, storage } from '../firebase'; // Ensure you have storage in your firebase exports
+import { Ionicons } from '@expo/vector-icons'; // Import Ionicons from Expo
+
+const screenWidth = Dimensions.get('window').width;
+const fieldWidth = screenWidth * 0.8; // 80% of the screen width
 
 const AddMemoryWidget = ({ isVisible, goalText, onClose }) => {
   const [title, setTitle] = useState('');
@@ -19,8 +23,8 @@ const AddMemoryWidget = ({ isVisible, goalText, onClose }) => {
       quality: 1,
     });
 
-    if (!result.cancelled) {
-      setImage(result.uri);
+    if (!result.canceled && result.assets) {
+      setImage(result.assets[0].uri); // Accessing the selected image through the assets array
     }
   };
 
@@ -33,26 +37,50 @@ const AddMemoryWidget = ({ isVisible, goalText, onClose }) => {
 
     let result = await ImagePicker.launchCameraAsync();
 
-    if (!result.cancelled) {
-      setImage(result.uri);
+    if (!result.canceled && result.assets) {
+      setImage(result.assets[0].uri); // Accessing the captured image through the assets array
     }
   };
 
+  const showActionSheet = () => {
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['Cancel', 'Pick from Camera Roll', 'Take a Photo'],
+        cancelButtonIndex: 0,
+      },
+      async (buttonIndex) => {
+        if (buttonIndex === 1) {
+          await pickImage();
+        } else if (buttonIndex === 2) {
+          await takePhoto();
+        }
+      }
+    );
+  };
+
   const uploadImage = async (uri) => {
-    const response = await fetch(uri);
-    const blob = await response.blob();
-    const fileRef = ref(storage, `memories/${new Date().toISOString()}`);
-    await uploadBytes(fileRef, blob);
-    return await getDownloadURL(fileRef);
+    try {
+      const response = await fetch(uri);
+      if (!response.ok) {
+        throw new Error('Failed to upload image');
+      }
+      const blob = await response.blob();
+      const fileRef = ref(storage, `memories/${new Date().toISOString()}`);
+      await uploadBytes(fileRef, blob);
+      return await getDownloadURL(fileRef);
+    } catch (error) {
+      console.error('Error uploading image: ', error);
+      throw error; // Re-throw the error to propagate it to the caller
+    }
   };
 
   const addMemoryToFirestore = async () => {
     if (image) {
-      const imageUrl = await uploadImage(image);
-      const auth = getAuth();
-      const user = auth.currentUser;
-      if (user) {
-        try {
+      try {
+        const imageUrl = await uploadImage(image);
+        const auth = getAuth();
+        const user = auth.currentUser;
+        if (user) {
           await addDoc(collection(db, 'memories'), {
             uid: user.uid,
             image_url: imageUrl,
@@ -63,15 +91,22 @@ const AddMemoryWidget = ({ isVisible, goalText, onClose }) => {
           console.log('Memory added successfully');
           onClose(); // Reset and close the modal after adding the memory
           setTitle(''); // Reset title
-          setDescription(`I have completed my goal: ${goalText}`); // Reset description to initial state
+          setDescription(''); // Reset description
           setImage(null); // Reset image
-        } catch (error) {
-          console.error('Error adding memory: ', error);
         }
+      } catch (error) {
+        console.error('Error adding memory: ', error);
+        Alert.alert("Error", "Failed to add memory. Please try again later.");
       }
     } else {
       Alert.alert("Please select an image for the memory.");
     }
+  };
+  
+
+  const handleClose = () => {
+    setImage(null); // Reset image when the close button is clicked
+    onClose(); // Close the modal
   };
 
   return (
@@ -83,7 +118,19 @@ const AddMemoryWidget = ({ isVisible, goalText, onClose }) => {
     >
       <View style={styles.centeredView}>
         <View style={styles.modalView}>
-          <Text style={styles.modalText}>Add this goal to your memories:</Text>
+          <TouchableOpacity style={styles.closeButton} onPress={handleClose}>
+            <Ionicons name="close" size={24} color="white" />
+          </TouchableOpacity>
+
+          <Text style={styles.modalText}>New Memory</Text>
+
+          {image ? (
+            <Image source={{ uri: image }} style={styles.imagePreview} />
+          ) : (
+            <TouchableOpacity style={styles.imagePlaceholder} onPress={showActionSheet}>
+              <Text style={styles.placeholderText}>Tap here to add an image</Text>
+            </TouchableOpacity>
+          )}
 
           <TextInput
             style={styles.input}
@@ -93,22 +140,15 @@ const AddMemoryWidget = ({ isVisible, goalText, onClose }) => {
           />
 
           <TextInput
-            style={styles.input}
+            style={styles.descriptionInput}
             onChangeText={setDescription}
             value={description}
             placeholder="Description"
             multiline
           />
 
-          <Button title="Pick an image from camera roll" onPress={pickImage} />
-          <Button title="Take a photo" onPress={takePhoto} />
-
           <TouchableOpacity style={styles.addButton} onPress={addMemoryToFirestore}>
             <Text style={styles.textStyle}>Add to Memories</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-            <Text style={styles.textStyle}>Close</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -124,10 +164,10 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0,0.5)',
   },
   modalView: {
-    margin: 20,
+    margin: 10,
     backgroundColor: 'white',
     borderRadius: 8,
-    padding: 35,
+    padding: 15,
     alignItems: 'center',
     shadowColor: '#000',
     shadowOffset: {
@@ -139,11 +179,24 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   modalText: {
-    marginBottom: 15,
+    fontFamily: 'PermanentMarker',
+    fontSize: 25,
+    marginBottom: 20,
     textAlign: 'center',
   },
   input: {
-    width: '100%',
+    fontFamily: 'PermanentMarker',
+    width: fieldWidth,
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    borderRadius: 5,
+  },
+  descriptionInput: {
+    fontFamily: 'PermanentMarker',
+    width: fieldWidth,
+    height: 100,
     marginBottom: 10,
     padding: 10,
     borderWidth: 1,
@@ -151,22 +204,50 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   addButton: {
-    backgroundColor: '#2196F3',
+    width: '100%',
+    backgroundColor: 'black',
     borderRadius: 20,
     padding: 10,
     elevation: 2,
-    marginBottom: 10,
   },
   closeButton: {
-    backgroundColor: 'grey',
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
   },
   textStyle: {
+    fontFamily: 'PermanentMarker',
     color: 'white',
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  imagePreview: {
+    width: 250,
+    height: 250,
+    marginBottom: 10,
+    borderRadius: 8,
+  },
+  imagePlaceholder: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 250,
+    height: 250,
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  placeholderText: {
+    fontFamily: 'PermanentMarker',
+    fontSize: 16,
+    color: '#666666',
   },
 });
 
